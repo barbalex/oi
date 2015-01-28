83d49402fd84d2b7bbb1ca8affcfdb341edc756d
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-01-28 17:18:54 +0100

    nav baut Baum mit Testdaten auf

--- src/oi_browserified.js
+++ src/oi_browserified.js
@@ -33637,25 +33637,96 @@ function mapObjects(doc) {
     }
 }
 
-function mapHierarchyToJstreeData(hierarchy) {
-    // _id wird id
-    hierarchy.id = hierarchy._id;
-    delete hierarchy._id;
-    // parent
+function createObjectsData(object, results, objectsData) {
+    if (object.parent !== null) {
+        var jstreeObject = {};
+        // _id wird id
+        jstreeObject.id = object._id;
+        // text is nameField
+        var h = _.find(results.hierarchies, function (hierarchy) {
+            return hierarchy._id == object.hId;
+        });
+        // beschirfte object
+        if (object.data && h && h.nameField) {
+            jstreeObject.text = object.data[h.nameField];
+        } else {
+            jstreeObject.text = '(?)';
+        }
+        // parent ist ein descendant hierarchy, ausser in der obersten Ebene
+        jstreeObject.parent = object.parent + h._id;
+        objectsData.push(jstreeObject);
+    }
 }
 
-function mapObjectToJstreeData(object) {
-    // _id wird id
-    object.id = object._id;
-    delete object._id;
-    // parent ist das Hierarchieobjekt
-    object.parent = object.hId;
-    delete object.type;
-    delete object.users;
-    delete object.editedBy;
-    delete object.data;
-    delete object._rev;
-    return object;
+function createTopObjectsData(objects, results) {
+    var topObjects = _.filter(objects, function (object) {
+        return object.parent === null;
+    });
+    var topObjectsArray = [];
+
+    _.each(topObjects, function (object) {
+        var jstreeObject = {};
+        // _id wird id
+        jstreeObject.id = object._id;
+        // text is nameField
+        var h = _.find(results.hierarchies, function (hierarchy) {
+            return hierarchy._id == object.hId;
+        });
+        // beschirfte object
+        if (object.data && h && h.nameField) {
+            jstreeObject.text = object.data[h.nameField];
+        } else {
+            jstreeObject.text = '(?)';
+        }
+        // parent ist ein descendant hierarchy, ausser in der obersten Ebene
+        jstreeObject.parent = '#';
+        topObjectsArray.push(jstreeObject);
+    });
+
+    return topObjectsArray;
+}
+
+// creates descendant hierarchical objects of single objects
+// adds them to an array
+function createDescendantHierarchiesOfObject(object, hierarchies, descendantHierarchiesData) {
+    // look for descendant hierarchies
+    var descendantHierarchies = _.filter(hierarchies, function (hierarchy) {
+        return hierarchy.parent !== null && hierarchy.parent == object.hId && _.indexOf(hierarchy.projIds, object.projId) > -1;
+    });
+
+    _.each(descendantHierarchies, function (hierarchy) {
+        var jstreeHierarchy = {};
+        // _id wird id
+        jstreeHierarchy.id = object._id + hierarchy._id;
+        // parent
+        jstreeHierarchy.parent = object._id;
+        // text is name
+        jstreeHierarchy.text = hierarchy.name || '(?)';
+        // add to array
+        descendantHierarchiesData.push(jstreeHierarchy);
+    });
+}
+
+function createTopHierarchies(hierarchies) {
+    // look for descendant hierarchies
+    var topHierarchies = _.filter(hierarchies, function (hierarchy) {
+        return hierarchy.parent === null;
+    });
+
+    var topHierarchiesData = [];
+
+    _.each(topHierarchies, function (hierarchy) {
+        var jstreeHierarchy = {};
+        // _id wird id
+        jstreeHierarchy.id = hierarchy._id;
+        // parent
+        jstreeHierarchy.parent = '#';
+        // text is name
+        jstreeHierarchy.text = hierarchy.name || '(?)';
+        topHierarchiesData.push(jstreeHierarchy);
+    });
+
+    return topHierarchiesData;
 }
 
 module.exports = function () {
@@ -33687,43 +33758,27 @@ module.exports = function () {
         if (err) { return console.log('error: ', err); }
         // results equals to: { hierarchies: hierarchies, objects: objects }
 
-        var data = [];
-
-        console.log('results.hierarchies: ', results.hierarchies);
-        console.log('results.objects: ', results.objects);
-
-        // höchste Hierarchieebene holen
-        /*var hierarchy0 = _.find(results.hierarchies, function (hierarchy) {
-            return hierarchy.parent === null;
-        });*/
-
         // Daten für jstree aufbereiten
         // alle Objekte holen
-        var objectData = _.map(results.objects, mapObjectToJstreeData);
-
-        console.log('objectData after map: ', objectData);
-
-        _.each(objectData, function (data) {
-            var h = _.find(results.hierarchies, function (hierarchy) {
-                return hierarchy._id == data.parent;
-            });
-            if (data.data && h && h.nameField) {
-                data.text = data.data[h.nameField];
-            } else {
-                data.text = '(?)';
-            }
+        var objectsData = [];
+        _.each(results.objects, function (object) {
+            createObjectsData(object, results, objectsData);
         });
+        console.log('objectsData: ', objectsData);
 
-        console.log('objectData after find: ', objectData);
+        var descendantHierarchiesData = [];
+        _.each(results.objects, function (object) {
+            createDescendantHierarchiesOfObject(object, results.hierarchies, descendantHierarchiesData);
+        });
+        console.log('descendantHierarchiesData: ', descendantHierarchiesData);
 
-        // hierarchie holen
-
-        // name als text ergänzen
+        var topObjectsData = createTopObjectsData(objects, results);
+        console.log('topObjectsData: ', topObjectsData);
 
         $('#navContent').jstree({
             'plugins': ['wholerow', 'unique', 'dnd', 'state', 'contextmenu'],
             'core': {
-                'data': objectData,
+                'data': _.union(objectsData, descendantHierarchiesData, topObjectsData),
                 'themes': {
                     'responsive': true,
                     'icons':      false,
