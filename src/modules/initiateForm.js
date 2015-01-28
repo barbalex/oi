a44f778a630056645ef98475a9b9072544a7bfa8
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-01-28 22:27:55 +0100

    Code aufgeräumt

--- src/modules/initiateForm.js
+++ src/modules/initiateForm.js
@@ -13,15 +13,12 @@ module.exports = function (_id) {
 
     console.log('_id: ', _id);
 
+    // get data for object
     db.get(_id, function (err, object) {
         if (err) { console.log('error: ', err); }
-        //console.log('object: ', object);
-        $('#formContent').html(JSON.stringify(object));
-        console.log('object.hId: ', object.hId);
         // get metainformation about fields
         db.get(object.hId, function (err, hierarchy) {
             if (err) { console.log('error: ', err); }
-            console.log('hierarchy: ', hierarchy);
             _.each(hierarchy.fields, function (field) {
                 var templateObject = {};
                 templateObject.objectId = object._id;
@@ -30,8 +27,6 @@ module.exports = function (_id) {
                 templateObject.value = object.data[field.label] || null;
                 html += input(templateObject);
             });
-
-            //html += '</form>';
 
             $('#formContent').html(html);
         });
