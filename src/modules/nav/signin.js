fbc51a708600629c16c69d5878bd82d1a9f2643c
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-02-06 20:49:54 +0100

    login weiter

--- src/modules/nav/signin.js
+++ src/modules/nav/signin.js
@@ -11,12 +11,33 @@ module.exports = function () {
     $('#signinWithModal').modal();
 
     // event-handler erzeugen
+    $('body').on('click', '#signinWithModalLogin', function () {
+        // signin oder signup?
 
-    // login in DB speichern
-    db.put(login, '_local/login').then(function () {
-        window.oi.login = login.name;
-        goOn();
-    }).catch(function (err) {
-        console.log('error creating login: ', err);
+        // Eingaben verifizieren
+
+        // event-listener entfernen
+        $('body').off('click', '#signinWithModalLogin');
+
+        // wenn signup: zuerst signup
+        db.signup('test@test.ch', 'myPassword', {
+            metadata: {
+                Nachname: 'Tester',
+                Vorname: 'Test',
+                Strasse: 'x-str 40',
+                PLZ: 8000,
+                Ort: 'ort'
+            }
+        }, function (err, response) {
+            // etc.
+        });
+
+        // login in DB speichern
+        db.put(login, '_local/login').then(function () {
+            window.oi.login = login.name;
+            goOn();
+        }).catch(function (err) {
+            console.log('error creating login: ', err);
+        });
     });
 };
