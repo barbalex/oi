5e24d263b8fa0da0d362da31aba26303cc7fdf4a
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-03-14 10:54:20 +0100

    signin: signup > focus password 2

--- src/oi_browserified.js
+++ src/oi_browserified.js
@@ -43013,6 +43013,10 @@ module.exports = function () {
     if ($(this).is(':checked')) {
         $('#signinWithModalPassword2').parent().show();
         $('#signinWithModalSigninButton').text('Konto erstellen und anmelden');
+        // wenn Email und Passwort erfasst sind, wiederholen fokussieren
+        if ($('#signinWithModalEmail').val() && $('#signinWithModalPassword').val()) {
+            $('#signinWithModalPassword2').focus();
+        }
     } else {
         $('#signinWithModalPassword2').parent().hide();
         $('#signinWithModalSigninButton').text('anmelden');
--- src/oi_built.js
+++ src/oi_built.js
@@ -71202,6 +71202,10 @@ module.exports = function () {
     if ($(this).is(':checked')) {
         $('#signinWithModalPassword2').parent().show();
         $('#signinWithModalSigninButton').text('Konto erstellen und anmelden');
+        // wenn Email und Passwort erfasst sind, wiederholen fokussieren
+        if ($('#signinWithModalEmail').val() && $('#signinWithModalPassword').val()) {
+            $('#signinWithModalPassword2').focus();
+        }
     } else {
         $('#signinWithModalPassword2').parent().hide();
         $('#signinWithModalSigninButton').text('anmelden');
