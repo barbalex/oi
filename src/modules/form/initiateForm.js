5032c2dd5ade2febefe789533caf9a71aee7e514
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-05-11 17:57:21 +0200

    initiateForm: modularisiert

--- src/modules/form/initiateForm.js
+++ src/modules/form/initiateForm.js
@@ -3,26 +3,17 @@
 
 var $                     = require('jquery'),
     _                     = require('underscore'),
-    ol                    = require('openlayers'),
-    input                 = require('../../../templates/input'),
     textarea              = require('../../../templates/textarea'),
-    checkbox              = require('../../../templates/checkbox'),
-    optionGroup           = require('../../../templates/optionGroup'),
-    checkboxGroup         = require('../../../templates/checkboxGroup'),
-    select                = require('../../../templates/select'),
-    geoJson               = require('../../../templates/geoJson'),
     formButtonToolbar     = require('../../../templates/formButtonToolbar'),
-    addCheckedToValueList = require('./addCheckedToValueList'),
     positionFormBtngroup  = require('./positionFormBtngroup'),
     getObject             = require('../getObject'),
     getHierarchy          = require('../getHierarchy'),
     resizeTextareas       = require('./resizeTextareas'),
     refreshScrollbar      = require('../refreshScrollbar'),
     capitalizeFirstLetter = require('../capitalizeFirstLetter'),
-    showTab               = require('../showTab'),
     showMap               = require('./showMap'),
-    zoomToFeatures        = require('../map/zoomToFeatures'),
-    setActiveObject       = require('../setActiveObject');
+    setActiveObject       = require('../setActiveObject'),
+    createHtmlForField    = require('./createHtmlForField');
 
 module.exports = function (id, type) {
     var html         = '',
@@ -30,9 +21,7 @@ module.exports = function (id, type) {
         object,
         hierarchy,
         $formContent = $('#formContent'),
-        geomFeatures = [],
-        geomFeature,
-        selectedFeatures;
+        geomFeatures = [];
 
     switch (type) {
     case 'object':
@@ -48,12 +37,15 @@ module.exports = function (id, type) {
                 });
                 _.each(hierarchy.fields, function (field) {
                     var value,
-                        templateObject                  = {};
+                        templateObject                  = {},
+                        data                            = {},
+                        returnData;
 
                     value                               = object.data[field.label];
                     templateObject.object               = {};
                     templateObject.object._id           = id;
                     templateObject.object.type          = type;
+                    templateObject.object.hId           = object.hId;
                     templateObject.object.inputType     = field.inputType;
                     templateObject.object.projId        = object.projId            || null;
                     templateObject.object.label         = field.label;
@@ -62,56 +54,19 @@ module.exports = function (id, type) {
                     templateObject.object.layerTitle    = hierarchy.name + ': ' + field.label;
                     templateObject.object.layerName     = 'layer' + capitalizeFirstLetter(hierarchy.name) + capitalizeFirstLetter(field.label);
 
-                    // Felder bauen
-                    switch (field.inputType) {
-                    case 'textarea':
-                        html += textarea(templateObject);
-                        textareaIds.push(id + field.label);
-                        break;
-                    case 'input':
-                        switch (field.inputDataType) {
-                        case 'checkbox':
-                            // es ist eine einzelne checkbox. Mitgeben, ob sie checked ist
-                            templateObject.checked = value ? 'checked' : '';
-                            html += checkbox(templateObject);
-                            break;
-                        case 'checkboxGroup':
-                            // checkboxGroup erstellen
-                            templateObject.object.valueList = addCheckedToValueList(field.valueList, value, 'checkboxGroup');
-                            html += checkboxGroup(templateObject);
-                            break;
-                        case 'optionGroup':
-                            templateObject.object.valueList = addCheckedToValueList(field.valueList, value, 'optionGroup');
-                            html += optionGroup(templateObject);
-                            break;
-                        case 'text':
-                        default:
-                            html += input(templateObject);
-                            break;
-                        }
-                        break;
-                    case 'select':
-                        // object.data muss Array sein - ist bei select nicht so, weil eh nur ein Wert gesetzt werden kann > Wert in Array setzen
-                        templateObject.object.valueList = addCheckedToValueList(field.valueList, value, 'select');
-                        html += select(templateObject);
-                        break;
-                    case 'geoJson':
-                        // Daten als JSON in textarea schreiben
-                        templateObject.object.value = value ? JSON.stringify(value, null, 4) : '';
-                        templateObject.object.hId   = object.hId;
-                        html += geoJson(templateObject);
-                        textareaIds.push(id + field.label);
-                        // prepare feature to zoom the map to
-                        if (value && value.type && value.coordinates) {
-                            geomFeature = new ol.Feature();
-                            geomFeature.setGeometry(new ol.geom[value.type](value.coordinates));
-                            geomFeatures.push(geomFeature);
-                        }
-                        break;
-                    default:
-                        html += input(templateObject);
-                        break;
-                    }
+                    // prepare data to pass to createHtmlForField
+                    data.html           = html;
+                    data.textareaIds    = textareaIds;
+                    data.geomFeatures   = geomFeatures;
+                    data.field          = field;
+                    data.templateObject = templateObject;
+
+                    returnData   = createHtmlForField(data);
+
+                    // extract return data from createHtmlForField
+                    html         = returnData.html;
+                    geomFeatures = returnData.geomFeatures;
+                    textareaIds  = returnData.textareaIds;
                 });
 
                 // add button toolbar
